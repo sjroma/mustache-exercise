d3b1f80013d8cd85085cbc092bfe103ca0ece12a
Steve Roma <sjroma@hotmail.com>
2017-06-21 15:12:09 -0400

    push mustache template toDo list

---
+++ data.js
@@ -0,0 +1,13 @@
+todo = [
+  {"listItem": "Learn Node basics"},
+  {"listItem": "Learn Express basics"},
+  {"listItem": "Learn Mustache"},
+  {"listItem": "Learn HTML forms with Express"},
+  {"listItem": "Learn about authentication"},
+  {"listItem": "Learn how to connect to PostgreSQL"},
+  {"listItem": "Learn how to create databases"},
+  {"listItem": "Learn SQL"},
+  {"listItem": "Learn how to connect to PostgreSQL from Node"},
+  {"listItem": "Learn how to use Sequelize"}
+]
+  
